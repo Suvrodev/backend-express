fa086f24b19bff50539e594382fd190bb093092b
Suvrodeb <suvrodevhowlader1408@gmail.com>
2025-08-16 23:32:07 +0600

    Perser json done

--- dist/app.js
+++ dist/app.js
@@ -6,9 +6,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // const express = require('express')
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+//For req.body - json
+app.use(express_1.default.json());
 const port = 7000;
 app.get("/", (req, res) => {
     res.send("Hello Back end by Nodemon");
+});
+app.post("/", (req, res) => {
+    const data = req.body;
+    console.log("Data:", data);
+    res.json({
+        message: "Successfully got data",
+    });
 });
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,21 @@
 // const express = require('express')
 import express, { Request, Response } from "express";
 const app = express();
+
+//For req.body - json
+app.use(express.json());
 const port = 7000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello Back end by Nodemon");
+});
+app.post("/", (req: Request, res: Response) => {
+  const data = req.body;
+
+  console.log("Data:", data);
+  res.json({
+    message: "Successfully got data",
+  });
 });
 
 app.listen(port, () => {
